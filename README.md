8ae60fbbdd06d4bec19f0efaee9fd369921a9d71
jiakai0419 <jiakai0419@gmail.com>
2016-03-27 01:06:46 +0800

    initial commit   Changes to be committed:  	new file:   .gitignore  	new file:   LICENSE  	new file:   README.md  	new file:   Setup.hs  	new file:   WYASI48H-NEW.cabal  	new file:   app/Main.hs  	new file:   src/Lib.hs  	new file:   stack.yaml  	new file:   stdlib.scm  	new file:   test/Spec.hs

---
+++ README.md
@@ -0,0 +1,2 @@
+# WYASI48H
+write yourself a scheme in 48 hours
---
+++ app/Main.hs
@@ -0,0 +1,9 @@
+module Main where
+
+import Lib
+import System.Environment
+
+main :: IO ()
+main = do
+    args <- getArgs
+    if null args then runRepl else runOne args
